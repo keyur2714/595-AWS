1e9d9768a7a166649952dc2d7de3e414f8ee306b
keyur2714 <keyurjava27@gmail.com>
2023-02-26 09:32:52 +0530

    Added CSS

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
 Hello Good Morning...!
 <h1>Hello Radhe Krishna...</h1>
 <ul>
@@ -6,3 +16,5 @@ Hello Good Morning...!
 	<li>Vinit</li>
 </ul>
 <h2>Hi....</h2>
+</body>
+</html>
